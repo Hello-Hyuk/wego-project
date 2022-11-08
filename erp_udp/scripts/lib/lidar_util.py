0529773c2e64cfb449ede77900abf018517003f9
Hello-Hyuk <jay@inha.edu>
2022-11-08 10:39:12 +0900

    code edit) make lidar class

--- erp_udp/scripts/lib/lidar_util.py
+++ erp_udp/scripts/lib/lidar_util.py
@@ -105,39 +105,38 @@ class UDP_LIDAR_Parser :
         
 class PCD:
     def __init__(self):
-        self.pcd = o3d.geometry.PointCloud()
+        self.pcd =  o3d.geometry.PointCloud()
         self.pcd_np = None
-        self.ROIheight = 18
-        self.ROIwidth = 6
     
     def point_np2pcd(self, points_np):
-        self.pcd_np = points_np.T
-        self.pcd = o3d.utility.Vector3dVector(self.pcd_np)
+        self.pcd_np = points_np.T        
+        self.pcd.points = o3d.utility.Vector3dVector(self.pcd_np)
 
     def Voxelize(self):
         print(f"Points before downsampling: {len(self.pcd.points)} ")
         # Points before downsampling: 115384 
         self.pcd = self.pcd.voxel_down_sample(voxel_size=0.2)
         print(f"Points after downsampling: {len(self.pcd.points)}")
-        self.pcd_np = np.asarray(self.pcd)
+        self.pcd_np = np.asarray(self.pcd.points)
     
     def Display_pcd(self):
         o3d.visualization.draw_geometries([self.pcd])
 
-    def ROI_filtering(self):
+    def ROI_filtering(self,ROIheight,ROIwidth):
         #point shape (3,)
         points = self.pcd_np.T
+
         points = np.delete(points,np.where(points[2,:]<-0.5),axis=1)
         points = np.delete(points,np.where(points[2,:]>0.7),axis=1)
         
-        points = np.delete(points,np.where(points[1,:]>self.ROIheight),axis=1)
+        points = np.delete(points,np.where(points[1,:]>ROIheight),axis=1)
         points = np.delete(points,np.where(points[1,:]<1),axis=1)
         
-        points = np.delete(points,np.where(points[0,:]>self.ROIwidth),axis=1)
-        points = np.delete(points,np.where(points[0,:]<-self.ROIwidth),axis=1)
+        points = np.delete(points,np.where(points[0,:]>ROIwidth),axis=1)
+        points = np.delete(points,np.where(points[0,:]<-ROIwidth),axis=1)
 
         self.pcd_np = points.T
-        self.pcd = self.point_np2pcd(points)
+        self.point_np2pcd(points)
 
 def DBscan(points):
     # create model and prediction
@@ -147,19 +146,23 @@ def DBscan(points):
         idx = np.where(labels==label)
         center_point.append(get_center_point(points,idx))
         
-    print(set(labels))
+    # print(set(labels))
     # Number of clusters in labels, ignoring noise if present.
     n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
     n_noise_ = list(labels).count(-1)
     
-    # labelcount = len(np.unique(labels))
-    print("Estimated number of clusters: %d" % n_clusters_)
-    print("Estimated number of noise points: %d" % n_noise_)
-    return center_point
+    # # labelcount = len(np.unique(labels))
+    # print("Estimated number of clusters: %d" % n_clusters_)
+    # print("Estimated number of noise points: %d" % n_noise_)
+    return n_clusters_, center_point
 
 def get_center_point(points,idx):
     point = np.mean(points[idx,:],axis=1)
     return point
+
+def print_i_d(intensity, distance):
+    print('raw distance shape',(distance).shape)
+    print('raw intensity shape',(intensity).shape)
     
 def printData(obj_data, position_x, position_y, position_z, center_points_np, ego_np):
     # print(f"ego : {position_x,position_y,position_z}")
--- erp_udp/scripts/lidar.py
+++ erp_udp/scripts/lidar.py
@@ -4,7 +4,7 @@ import cv2
 import numpy as np
 import time
 import matplotlib.pyplot as plt
-from lib.lidar_util import UDP_LIDAR_Parser, ROI_filtering, DBscan, get_center_point, printData, Dis_PointCloud_np, PCD
+from lib.lidar_util import UDP_LIDAR_Parser, DBscan, get_center_point, printData, Dis_PointCloud_np, PCD
 from lib.morai_udp_parser import udp_parser
 import os,json
 import open3d as o3d
@@ -33,17 +33,25 @@ full_path = path+'/'+path_file_name
 full_path_slice = path+'/'+path_file_slice
 f=open(full_path, 'w')
 f=open(full_path_slice, 'w')
-np.set_printoptions(precision=5)
+
+class LIDAR():
+    def __init__(self):
+        self.udp_lidar = UDP_LIDAR_Parser(ip=params_lidar["localIP"], port=params_lidar["localPort"], params_lidar=params_lidar)
+        self.pcd_info = PCD()
+        self.n_clusters = 0
+        self.cluster_coords = None
+    def display_info(self):
+        print(f"number of point cloud data : {self.pcd_info.pcd_np.shape}\n")
+        print(f"number of cluster : {self.n_clusters}\ncluster coordinate : {self.cluster_coords}\n")
+
 def main():
     cnt = 0
-    udp_lidar = UDP_LIDAR_Parser(ip=params_lidar["localIP"], port=params_lidar["localPort"], params_lidar=params_lidar)
+    lidar = LIDAR()
+    lidar_pcd = PCD()
     obj=udp_parser(user_ip, params["object_info_dst_port"],'erp_obj')
     ego=udp_parser(user_ip, params["vehicle_status_dst_port"],'erp_status')
-    lidar_pcd = PCD()
-    height = 18
-    width = 6
     while True :
-        if udp_lidar.is_lidar ==True:
+        if lidar.udp_lidar.is_lidar ==True:
             # data parsing
             ######## obj info
             #obj_info_list = [obj_id, obj_type, pos_x, pos_y, pos_z, heading, size_x, size_y, size_z
@@ -59,9 +67,9 @@ def main():
             position_y=round(status_data[13],5)
             position_z=round(status_data[14],5)
             ######## lidar data
-            x=udp_lidar.x
-            y=udp_lidar.y
-            z=udp_lidar.z
+            x=lidar.udp_lidar.x
+            y=lidar.udp_lidar.y
+            z=lidar.udp_lidar.z
 
             sim_x = obj_x-position_x
             sim_y = obj_y-position_y
@@ -76,29 +84,31 @@ def main():
             #point cloud (3,57600)
             print("point clouds",points,points.shape)
             
+            
+            
             #numpy to point cloud data
-            lidar_pcd.np2pcd(points)
+            lidar.pcd_info.point_np2pcd(points)
             #voxelize
-            lidar_pcd.Voxelize()
-            lidar_pcd.Display_pcd()
-            
-            # point ROI 기반 filtering 진행
-            points = ROI_filtering(height, width, points)
-            
-            if points in points:
+            lidar.pcd_info.Voxelize()
+            height = 18
+            width = 6
+            lidar.pcd_info.ROI_filtering(height,width)
+            lidar.pcd_info.Display_pcd()
+            if lidar.pcd_info.pcd.points :
                 # points shape (,3)
-                center_points = DBscan(points)
+                n_clusters_, center_points = DBscan(lidar.pcd_info.pcd_np)
                 center_points_np = np.array(center_points)
                 center_points_np = np.squeeze(center_points)
-                #ego_np = np.array([position_x,position_y,position_z])
+                lidar.n_clusters = n_clusters_
+                lidar.cluster_coords = center_points_np
                 
-                Dis_PointCloud_np(points)
+                #ego_np = np.array([position_x,position_y,position_z])
+                lidar.display_info()
+                #Dis_PointCloud_np(points)
                 time.sleep(1)
             else : pass
 
-def print_i_d(intensity, distance):
-    print('raw distance shape',(distance).shape)
-    print('raw intensity shape',(intensity).shape)
+
         
 if __name__ == '__main__':
 
