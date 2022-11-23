8d4299a895590398f53bb13570f48c698d863f67
jay <jay@inha.edu>
2022-11-23 15:14:28 +0900

    code add) get 3d bounding box center coord

--- erp_udp/scripts/lidar.py
+++ erp_udp/scripts/lidar.py
@@ -42,18 +42,20 @@ class LIDAR():
 
             #numpy to point cloud data
             self.pcd_info.point_np2pcd(points)
+            self.pcd_info.get_origin_point(points)
             #voxelize
             self.pcd_info.Voxelize()
             
-            height,width = 10,3
+            height,width = 15,3
             #ROI filtering
             self.pcd_info.ROI_filtering(height,width)
             
             # DBscan clustering
             if self.pcd_info.pcd.points :
                 # points shape (,3)
-                self.pcd_info.Display_pcd()
+                #self.pcd_info.Display_pcd(self.pcd_info.pcd)
                 self.n_clusters, self.cluster_coords = self.pcd_info.o3d_DBscan()
+                
                 time.sleep(1)
             else : 
                 self.n_clusters, self.cluster_coords = 0, None
@@ -66,13 +68,20 @@ class LIDAR():
 
 class PCD:
     def __init__(self):
+        self.origin_pcd = o3d.geometry.PointCloud()
         self.pcd =  o3d.geometry.PointCloud()
         self.pcd_np = None
+        self.pcd_center = []
+    
+    def get_origin_point(self,points_np):
+        self.pcd_np = points_np.T        
+        self.origin_pcd.points = o3d.utility.Vector3dVector(self.pcd_np)
+
     
     def point_np2pcd(self, points_np):
         self.pcd_np = points_np.T        
         self.pcd.points = o3d.utility.Vector3dVector(self.pcd_np)
-
+        
     def Voxelize(self):
         #print(f"Points before downsampling: {len(self.pcd.points)} ")
         # Points before downsampling: 115384 
@@ -80,8 +89,8 @@ class PCD:
         #print(f"Points after downsampling: {len(self.pcd.points)}")
         self.pcd_np = np.asarray(self.pcd.points)
     
-    def Display_pcd(self):
-        o3d.visualization.draw_geometries([self.pcd])
+    def Display_pcd(self, pcd):
+        o3d.visualization.draw_geometries([pcd])
         
     def Write_pcd(self, file_name):
         output_file = file_name
@@ -113,63 +122,110 @@ class PCD:
         self.pcd_np = points.T
         self.point_np2pcd(points)
         
-    def DBscan(self):
-        # create model and prediction
-        centroid, labels = dbscan(self.pcd_np, eps=1.0, min_samples=10)
-        # Number of clusters in labels, ignoring noise if present.
-        n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-        n_noise_ = list(labels).count(-1)
-        
-        center_point = []
-        for label in range(len(set(labels[labels!=-1]))):
-            idx = np.where(labels==label)
-            center_point.append(np.mean(self.pcd_np[idx,:],axis=1))
-            
-        center_points_np = np.array(center_point)
-        center_points_np = np.squeeze(center_points_np)
-        
-        if len(center_points_np) == 3:
-            center_points_sorted = center_points_np
-        else :
-            try : center_points_sorted = center_points_np[center_points_np[:,1].argsort()]
-            # Morai respwan bug exception
-            except IndexError:
-                center_points_sorted = center_points_np
-                
-        # print(set(labels))
-        # print("Estimated number of clusters: %d" % n_clusters_)
-        # print("Estimated number of noise points: %d" % n_noise_)
-        return n_clusters_, center_points_sorted
     
+    def get_pcd_center(self,idx):
+        # np2pcd
+        pcd =  o3d.geometry.PointCloud()
+        pcd_center = o3d.geometry.PointCloud()
+        pcd_bbox_center = o3d.geometry.PointCloud()
+        
+        pcd_np = self.pcd_np[idx,:]   
+        pcd_np = np.squeeze(pcd_np)  
+        pcd.points = o3d.utility.Vector3dVector(pcd_np)  
+        pcd_center_np = pcd.get_center() 
+        pcd_center_np = pcd_center_np[np.newaxis]
+        pcd_center.points = o3d.utility.Vector3dVector(pcd_center_np) 
+        
+        pcd_bbox = pcd.get_axis_aligned_bounding_box()  
+        pcd_bbox_center_np = pcd_bbox.get_center()
+        pcd_center_np = pcd.get_center() 
+        
+        #pcd_bbox_center_np = pcd_bbox_center_np[np.newaxis]
+        # pcd_center_np = pcd_center_np[np.newaxis]
+        # pcd_bbox_center.points = o3d.utility.Vector3dVector(pcd_bbox_center_np) 
+        # pcd_center.points = o3d.utility.Vector3dVector(pcd_center_np) 
+          
+        pcd_bbox.color = (1, 0, 0)
+
+        #o3d.visualization.draw_geometries([pcd, pcd_bbox_center, pcd_bbox,pcd_center])
+
+        return pcd_bbox_center_np, pcd_bbox
+        
     def o3d_DBscan(self):
             # create model and prediction
             self.labels = self.pcd.cluster_dbscan(eps=1.0, min_points=10)
             # Number of clusters in labels, ignoring noise if present.
             n_clusters_ = len(set(self.labels)) - (1 if -1 in self.labels else 0)
             n_noise_ = list(self.labels).count(-1)
+            self.labels = np.asarray(self.labels)
             
             center_point = []
-            self.labels = np.asarray(self.labels)
-        
+            pcd_center = []
+            pcd_bbox = []
             for label in range(len(set(self.labels[self.labels!=-1]))):
                 idx = np.where(self.labels==label)
                 center_point.append(np.mean(self.pcd_np[idx,:],axis=1))
-                
+                pc, pb = self.get_pcd_center(idx)
+                pcd_center.append(pc)
+                pcd_bbox.append(pb)
+          
             center_points_np = np.array(center_point)
+            pcd_center_np = np.array(pcd_center)
             center_points_np = np.squeeze(center_points_np)
+
+            # (n,3)
+            # print(pcd_center_np.shape)
+            # print(center_points_np.shape)
             
-            if len(center_points_np) == 3:
-                center_points_sorted = center_points_np
+            if pcd_center_np.shape[0] == 1:
+                pcd_center_sorted = pcd_center_np
             else :
-                try : center_points_sorted = center_points_np[center_points_np[:,1].argsort()]
+                try : pcd_center_sorted = pcd_center_np[pcd_center_np[:,1].argsort()]
                 # Morai respwan bug exception
                 except IndexError:
-                    center_points_sorted = center_points_np
+                    pcd_center_sorted = pcd_center_np
+                    
+            # if len(center_points_np) == 3:
+            #     center_points_sorted = center_points_np
+            # else :
+            #     try : center_points_sorted = center_points_np[center_points_np[:,1].argsort()]
+            #     # Morai respwan bug exception
+            #     except IndexError:
+            #         center_points_sorted = center_points_np
                     
             # print(set(labels))
             # print("Estimated number of clusters: %d" % n_clusters_)
             # print("Estimated number of noise points: %d" % n_noise_)
-            return n_clusters_, center_points_sorted
+            return n_clusters_, pcd_center_sorted
+        
+def DBscan(self):
+    # create model and prediction
+    centroid, labels = dbscan(self.pcd_np, eps=1.0, min_samples=10)
+    # Number of clusters in labels, ignoring noise if present.
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+    n_noise_ = list(labels).count(-1)
+    
+    center_point = []
+    for label in range(len(set(labels[labels!=-1]))):
+        idx = np.where(labels==label)
+        center_point.append(np.mean(self.pcd_np[idx,:],axis=1))
+        
+    center_points_np = np.array(center_point)
+    center_points_np = np.squeeze(center_points_np)
+    
+    if len(center_points_np) == 3:
+        center_points_sorted = center_points_np
+    else :
+        try : center_points_sorted = center_points_np[center_points_np[:,1].argsort()]
+        # Morai respwan bug exception
+        except IndexError:
+            center_points_sorted = center_points_np
+            
+    # print(set(labels))
+    # print("Estimated number of clusters: %d" % n_clusters_)
+    # print("Estimated number of noise points: %d" % n_noise_)
+    return n_clusters_, center_points_sorted
+
 
 def main():
     lidar = LIDAR(params_lidar)
@@ -180,12 +236,12 @@ def main():
         if lidar.udp_lidar.is_lidar ==True:
             # data parsing
             ####### obj info
-            obj_data=obj.get_data()
-            obj_data_np = np.array(obj_data)
-            try : 
-                obj_coords = obj_data_np[:,2:5]
-            except IndexError :
-                obj_coords = obj_data_np[2:5]
+            # obj_data=obj.get_data()
+            # obj_data_np = np.array(obj_data)
+            # try : 
+            #     obj_coords = obj_data_np[:,2:5]
+            # except IndexError :
+            #     obj_coords = obj_data_np[2:5]
                 
             ####### ego info
             status_data = ego.get_data()
