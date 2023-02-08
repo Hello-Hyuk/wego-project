9dbae00fb62167e98f66ab5926083e080b207dfe
Hello-Hyuk <jay@inha.edu>
2023-02-08 09:22:47 +0900

    mouse callback

--- erp_udp/scripts/camera/opencv-examples/mouse_callback.py
+++ erp_udp/scripts/camera/opencv-examples/mouse_callback.py
@@ -3,9 +3,10 @@ import numpy as np
 
 # mouse callback function
 def draw_circle(event,x,y,flags,param):
-   if event == cv2.EVENT_LBUTTONDBLCLK:
-       cv2.circle(img,(x,y),100,(255,0,0),-1)
-       print(x,y)
+    if event == cv2.EVENT_MOUSEMOVE:
+    # if event == cv2.EVENT_LBUTTONDBLCLK:
+        cv2.circle(img,(x,y),10,(255,0,0),-1)
+        print(x,y)
 
 # Create a black image, a window and bind the function to window
 img = np.zeros((512,512,3), np.uint8)
